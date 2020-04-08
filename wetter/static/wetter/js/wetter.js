0a853e07b6795fef2dabc7995ce856d2ed489d18
Sebastian Speitel <sebastian.speitel@outlook.de>
2020-04-08 12:56:39 +0200

    use node resolution to resolve css

--- wetter/static/wetter/js/wetter.js
+++ wetter/static/wetter/js/wetter.js
@@ -1,18 +1,18 @@
 import "bootstrap";
-import "../../../../node_modules/bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 import "datatables";
-import "../../../../node_modules/datatables/media/css/jquery.dataTables.css";
+import "datatables/media/css/jquery.dataTables.css";
 
 import $ from "jquery";
 
 import "leaflet";
-import "../../../../node_modules/leaflet/dist/leaflet.css";
+import "leaflet/dist/leaflet.css";
 
 import "moment";
 
 import "tempusdominus-bootstrap-4";
-import "../../../../node_modules/tempusdominus-bootstrap-4/build/css/tempusdominus-bootstrap-4.css";
+import "tempusdominus-bootstrap-4/build/css/tempusdominus-bootstrap-4.css";
 
 import "../css/wetter.css";
 

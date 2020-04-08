b8c4b787179243fc13950e15a18fbfe82da396ca
Sebastian Speitel <sebastian.speitel@outlook.de>
2020-04-08 12:02:05 +0200

    include images in bundle output

--- rollup.config.js
+++ rollup.config.js
@@ -2,20 +2,29 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
 import css from "rollup-plugin-scss";
+import copy from "rollup-plugin-copy";
 
 const production = process.env.NODE_ENV === "production";
 
 const config = {
   input: "wetter/static/wetter/js/wetter.js",
   output: {
-    dir: "wetter/static/js",
+    dir: "wetter/static",
     format: "iife",
     sourcemap: !production
   },
   plugins: [
+    copy({
+      targets: [
+        {
+          src: "node_modules/leaflet/dist/images/*",
+          dest: "wetter/static/images"
+        }
+      ]
+    }),
     css({
-      output: "wetter/static/css/bundle.css",
-      outFile: "wetter/static/css/bundle.css",
+      output: "wetter/static/bundle.css",
+      outFile: "wetter/static/bundle.css",
       outputStyle: production ? "compressed" : "expanded",
       sourceMap: !production
     }),

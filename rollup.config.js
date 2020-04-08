fd8da4968c3f87f4c0ca8896f3c42ba41de37352
Sebastian Speitel <sebastian.speitel@outlook.de>
2020-04-08 11:44:18 +0200

    added css to rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,28 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
+import css from "rollup-plugin-scss";
 
 const production = process.env.NODE_ENV === "production";
 
-export default {
+const config = {
   input: "wetter/static/wetter/js/wetter.js",
   output: {
     dir: "wetter/static/js",
     format: "iife",
     sourcemap: !production
   },
-  plugins: [resolve(), commonjs(), !production && terser()]
+  plugins: [
+    css({
+      output: "wetter/static/css/bundle.css",
+      outFile: "wetter/static/css/bundle.css",
+      outputStyle: production ? "compressed" : "expanded",
+      sourceMap: !production
+    }),
+    resolve(),
+    commonjs(),
+    production && terser()
+  ]
 };
+
+export default [config];
--- wetter/static/wetter/js/wetter.js
+++ wetter/static/wetter/js/wetter.js
@@ -1,9 +1,20 @@
 import "bootstrap";
+import "../../../../node_modules/bootstrap/dist/css/bootstrap.css";
+
 import "datatables";
+import "../../../../node_modules/datatables/media/css/jquery.dataTables.css";
+
 import $ from "jquery";
+
 import "leaflet";
+import "../../../../node_modules/leaflet/dist/leaflet.css";
+
 import "moment";
+
 import "tempusdominus-bootstrap-4";
+import "../../../../node_modules/tempusdominus-bootstrap-4/build/css/tempusdominus-bootstrap-4.css";
+
+import "../css/wetter.css";
 
 // Entfernen des Clickoverlay wenn drauf geklickt wurde, #28
 $(".clickoverlay").on("click", function () {

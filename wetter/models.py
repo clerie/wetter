ccc89c80baf23548f1d35b2c56d2bcfe80897987
clerie <git@clerie.de>
2020-04-11 15:58:29 +0200

    Display age of data

--- wetter/models.py
+++ wetter/models.py
@@ -2,6 +2,8 @@
 
 from wetter import db
 
+from datetime import datetime, timedelta
+
 class Stations(db.Model):
     __tablename__ = 'stations'
     id = db.Column(db.Integer, primary_key=True)
@@ -12,6 +14,19 @@ class Stations(db.Model):
     dwd_last_update = db.Column(db.TIMESTAMP)
     state = db.Column(db.String)
     sea_level = db.Column(db.Integer)
+
+    def climate_date_range(self):
+        r = db.session.query(db.func.min(Climate.date), db.func.max(Climate.date)).filter_by(station=self.id).one()
+        return {"min": r[0], "max": r[1]}
+
+    def climate_date_old(self):
+        return self.climate_date_range()["max"] < (datetime.today().date() - timedelta(days=1))
+
+    def climate_count(self):
+        return db.session.query(db.func.count(Climate.id)).filter_by(station=self.id).one()[0]
+
+    def __str__(self):
+        return self.name
 
 class Climate(db.Model):
     __tablename__ = 'climate'
